406223ac394817955b3018381810eaf476e3df9f
Pooja Chandrikapure <30poojachandrikapure@gmail.com>
2026-05-06 17:27:04 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import './style.css'
+</script>
+
+<template>
+  <!-- <HelloWorld /> -->
+  <RouterView/>
+</template>
+<style scoped>
+body{
+  background-color: black;
+}
+</style>
